39badf3e0f37d2bf7df757567ab5df09cfe3c7e4
ConfusedSky <masamaedae@gmail.com>
2018-01-20 14:06:12 -0800

    Initial Commit

---
+++ Television/Assets/Failure.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Failure : MonoBehaviour {
+	public void Fail()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex, LoadSceneMode.Single);
+	}
+}
