f4e92616fe67c23c3e68693e53346a616413f6c1
ConfusedSky <masamaedae@gmail.com>
2018-01-22 16:46:23 -0800

    Added a reset button. And did some balancing to make the game more speed runner friendly
    Ie less unnecessary waiting

--- Television/Assets/Failure.cs
+++ Television/Assets/Failure.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityStandardAssets.Characters.FirstPerson;
 
 public class Failure : MonoBehaviour
 {
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            CheckpointManager.UpdateCheckpoint((new GameObject()).transform);
+            Fail();
+        }
+    }
+
 	public void Fail()
 	{
         SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex, LoadSceneMode.Single);
