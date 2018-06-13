33f6d900481aff49ff9696f19b76892ee49521c0
ConfusedSky <masamaedae@gmail.com>
2018-06-13 13:40:51 -0700

    Started implementing the camera movement script.
    * Has weird issues with some of the teleporters.

--- Television/Assets/Teleportato.cs
+++ Television/Assets/Teleportato.cs
@@ -1,23 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityStandardAssets.Characters.FirstPerson;
+﻿using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class Teleportato : MonoBehaviour {
 
-	public GameObject target;
+    public float distanceToRender = 10;
+    public GameObject target;
+    private Camera destinationCamera = null;
+    private GameObject player = null;
+    private GameObject playerCamera = null;
+
+    void Awake()
+    {
+        destinationCamera = target.GetComponentInChildren<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerCamera = player.GetComponentInChildren<Camera>().gameObject;
+    }
+
+    void Update()
+    {
+        Vector3 distance = playerCamera.transform.position - transform.position; 
+        if(distance.magnitude < distanceToRender)
+        {
+            destinationCamera.transform.localPosition = distance;
+            destinationCamera.gameObject.SetActive(true); 
+            Quaternion rot = Quaternion.Inverse(transform.rotation) *
+                playerCamera.transform.rotation;
+            Quaternion end = rot * target.transform.rotation;
+            destinationCamera.transform.rotation = end;
+        }
+        else
+        {
+            destinationCamera.gameObject.SetActive(false);
+        }
+    }
 
     void OnTriggerEnter(Collider collider)
     {
         Debug.Log("Should be teleporting");
         GameObject teleportee = collider.gameObject;
         teleportee.transform.position = target.transform.position;
-        teleportee.transform.rotation = target.transform.rotation;
+        //teleportee.transform.rotation = target.transform.rotation;
 
         if(teleportee.tag == "Player")
         {
-            teleportee.GetComponent<FirstPersonController>().SetRotation(target.transform);
+            Quaternion rot = Quaternion.Inverse(transform.rotation) *
+                player.transform.rotation;
+            Quaternion end = rot * target.transform.rotation;
+            GameObject g = new GameObject();
+            g.transform.rotation = end;
+            teleportee.GetComponent<FirstPersonController>().SetRotation(g.transform);
+            Destroy(g);
         }
     }
 }
